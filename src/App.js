650d1771b62db513f2cd1579d4e91e7da9bb0810
Hannah_Brannan <hannahbrannan@gmail.com>
2017-05-31 13:03:43 -0700

    update (App.js, index.js) create connection to intial info update (Cookies) Add cookie dependency

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import './App.less';
 import Column from './components/Column';
+import { CookiesProvider, withCookies, Cookies } from 'react-cookie';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +20,15 @@ class App extends Component {
           To get it, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          <Column title="TaskList1"/>
+          <ul>
+            {
+              this.props.initialData.map((title)=>
+                <li key={title}>
+                  <Column title={title}/>
+                </li>
+              )
+            }
+          </ul>
         </div>
       </div>
     );
